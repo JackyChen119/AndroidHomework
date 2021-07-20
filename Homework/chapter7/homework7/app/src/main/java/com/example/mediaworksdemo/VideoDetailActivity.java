229c5c762223b640c9e1e4807dd4bfbac8e15525
Dragon <1198131650@qq.com>
2021-07-21 00:48:23 +0800

    homework7

---
+++ Homework/chapter7/homework7/app/src/main/java/com/example/mediaworksdemo/VideoDetailActivity.java
@@ -0,0 +1,26 @@
+package com.example.mediaworksdemo;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class VideoDetailActivity extends AppCompatActivity {
+
+    String mockUrl = "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_detail);
+
+        VideoView videoView = (CustomVideoView)findViewById(R.id.custom_video_view);
+        videoView.setVideoURI(Uri.parse(mockUrl));
+        videoView.setMediaController(new MediaController(this));
+        videoView.start();
+    }
+}
